2547279df5443bf441220b3043c4bf2416e782ca
Viki <34596151+KisCsaj68@users.noreply.github.com>
2022-01-23 16:57:27 +0100

    add type hint to __all__

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,6 +1,10 @@
+
 """{{cookiecutter.package_name}} -
 {{cookiecutter.package_description}}"""
 
 __version__ = '{{cookiecutter.package_version}}'
 __author__ = '{{cookiecutter.author_name}} <{{cookiecutter.author_email}}>'
-__all__ = []
+from typing import Any, List
+
+
+__all__: List[Any] = []
